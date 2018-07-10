79e1aba3c106d94717317d490f9dce9c0af2f4af
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-10 11:17:01 -0500

    Finished initial set up of ArticlesTableView

---
+++ TopStories/ArticlesViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Zoe Berthold on 7/10/18.
+//  Copyright © 2018 Zoe Berthold. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UITableViewController {
+    
+    var articles = [[String: String]]()
+    var apiKey = ""
+    var source = [String: String]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Top Stories"
+        let query = "https://newsapi.org/v1/articles?" + "source=\(source["id"]!)&apiKey=\(apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["status"] == "ok" {
+                        self.parse(json: json)
+                        return
+                    }
+                }
+            }
+            self.loadError()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func parse(json: JSON){
+        for result in json["articles"].arrayValue{
+            let title = result["title"].stringValue
+            let url = result["url"].stringValue
+            let description = result["description"].stringValue
+            let article = ["title":title, "description":description, "url":url]
+            articles.append(source)
+        }
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+        }
+    }
+    
+    func loadError (){
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]
+        cell.detailTextLabel?.text = article["description"]
+        return cell
+    }
+}
+
