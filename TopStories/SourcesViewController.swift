91d54f0a9eef7077338420ebc7376a51e54de879
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-10 10:22:26 -0500

    set up api controls

--- TopStories/SourcesViewController.swift
+++ TopStories/SourcesViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
+    var sources = [[String: String]]()
+    let apiKey = "5d892509a49046a087917c466fa80d09"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
     }
 
     override func didReceiveMemoryWarning() {
